0c8fd1211430a1e37230504aeccca67e2e16a1e2
kemasmyunus <7a9tqplxjh@gmail.com>
2025-02-09 22:29:20 +0800

    update: belajar magic function invoke

---
+++ Invoke.php
@@ -0,0 +1,9 @@
+<?php
+include "data/MahouTsukai.php";
+
+$freiren = new MahouTsukai();
+$freiren->name = "freiren";
+$freiren->maho = "zoltorak";
+$freiren->toshi = 100000;
+
+$freiren(1,5,1,"haha");
--- data/MahouTsukai.php
+++ data/MahouTsukai.php
@@ -15,4 +15,9 @@ class MahouTsukai {
     public function __toString(){
         return "Mahou Tsukai wa namae wa $this->name desu, kore wa mahou wa $this->maho desu, soshite $this->toshi sai desu.";
     }
+
+    public function __invoke(...$arguments): void{
+        $join = join(",", $arguments);
+        echo "Invoke student with arguments $join".PHP_EOL;
+    }
 }
