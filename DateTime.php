21d61a03cd78084134d091f6794eeca7b86b0910
kemasmyunus <7a9tqplxjh@gmail.com>
2025-02-13 20:22:54 +0800

    update: belajar format datetime dan parse datetime

--- DateTime.php
+++ DateTime.php
@@ -11,3 +11,11 @@ $dateTime->add($minusOneMonth);
 
 $dateTime->setTimezone(new DateTimeZone("Asia/Jakarta"));
 var_dump($dateTime);
+echo $dateTime->format("Y-m-d H:i:s").PHP_EOL;
+
+$dateLagi = DateTime::createFromFormat("Y-m-d H:i:s", "2026-11-30 19:12:12", new DateTimeZone("Asia/Jakarta"));
+if($dateLagi){
+    var_dump($dateLagi);
+}else{
+    echo "format salah".PHP_EOL;
+}
